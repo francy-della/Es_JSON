6055b5db7dfbece7c96d91f89ac737147834696b
FRANCESCADELLAMANO <francesca.dellamano@mpolo.it>
2023-02-14 11:13:34 +0100

    Classe Squadra

--- mini-robotica/src/main/java/com/mycompany/mini/robotica/MiniRobotica.java
+++ mini-robotica/src/main/java/com/mycompany/mini/robotica/MiniRobotica.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
  *
  * @author JENNIFERPAGANIN
  */
+
 public class MiniRobotica {
     public static void main(String[] args) throws IOException {
         //Accesso al file json
