b87939c70c86153c69b20219a19294dbd546af72
FRANCESCADELLAMANO <francesca.dellamano@mpolo.it>
2023-02-14 11:10:46 +0100

    Classe Squadra

---
+++ mini-robotica/src/main/java/com/mycompany/mini/robotica/Squadra.java
@@ -0,0 +1,18 @@
+package com.mycompany.mini.robotica;
+
+/**
+ *
+ * @author FRANCESCADELLAMANO
+ */
+
+public class Squadra {
+    String nome;
+    int tempo;
+    int core;
+
+    public Squadra(String nome, int tempo, int core){
+        this.nome = nome;
+        this.tempo = tempo;
+        this.core = core;
+    }
+}
