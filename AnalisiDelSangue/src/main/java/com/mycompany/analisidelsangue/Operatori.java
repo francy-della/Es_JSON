bc5d2e25d63dbc0c9c15d2bd0be072b81b88a6b5
Jenny1804 <79317742+Jenny1804@users.noreply.github.com>
2023-02-15 01:26:24 +0100

    Classe operatori

---
+++ AnalisiDelSangue/src/main/java/com/mycompany/analisidelsangue/Operatori.java
@@ -0,0 +1,20 @@
+package com.mycompany.analisidelsangue;
+
+/**
+ *
+ * @author JENNIFERPAGANIN
+ */
+public class Operatori {
+    String codice;
+
+    public Operatori(String codice) {
+        this.codice = codice;
+    }
+
+    public String getCodice() {
+        return codice;
+    }
+    public void setCodice(String codice) {
+        this.codice = codice;
+    }
+}
